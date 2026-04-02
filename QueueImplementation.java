fb0b068ae72298d18a0e148091949db393b20741
Saihaj <saihajmann07@gmail.com>
2026-04-02 09:40:25 -0600

    queue initial

---
+++ QueueImplementation.java
@@ -0,0 +1,33 @@
+public class QueueImplementation<T> implements QueueInterface<T> {
+
+    @Override
+    public void enqueue(T newEntry) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'enqueue'");
+    }
+
+    @Override
+    public T dequeue() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dequeue'");
+    }
+
+    @Override
+    public T getFront() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getFront'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'clear'");
+    }
+
+}
