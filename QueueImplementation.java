56aa315889e014637a29e70255cac535306cece3
Saihaj <saihajmann07@gmail.com>
2026-04-02 11:51:12 -0600

    some testing

--- QueueImplementation.java
+++ QueueImplementation.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class QueueImplementation<T> implements QueueInterface<T> {
     LinkedListObj<T> dataQueue = new LinkedListObj<T>();
 
@@ -33,5 +35,25 @@ public class QueueImplementation<T> implements QueueInterface<T> {
     @Override
     public void clear() {
         this.dataQueue = new LinkedListObj<T>();
+    }
+
+    public static void main(String[] args) {
+        QueueImplementation<String> stringsTest = new QueueImplementation<>();
+        Scanner getInput = new Scanner(System.in);
+        while (true) {
+            String input = getInput.nextLine().trim();
+            if (input.isEmpty()) {
+                continue;
+            } else if (input.equalsIgnoreCase("break")) {
+                break;
+            } else if (input.equalsIgnoreCase("dequeue")) {
+                System.out.println("Dequeued: " + stringsTest.dequeue());
+            } else if (input.equalsIgnoreCase("front")) {
+                System.out.println("Front: " + stringsTest.getFront());
+            } else {
+                stringsTest.enqueue(input);
+            }
+        }
+        getInput.close();
     }
 }
